671c33e899f102fd57ad3328c5ddea9f9a175d53
Jade McDaniels <jademcdaniels81@gmail.com>
2020-12-29 01:59:33 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,8 +1,11 @@
 def find_element_index(scale,2)
   count = 0 
   scale = [2,3,4,5]
+  found_value_index = nil
   while count < scale.length do
-    puts array[count] == 2
+    if array[count] ==  value_to_find
+      found_value_index = count
    count +=1  
+    end
   end 
 end   
